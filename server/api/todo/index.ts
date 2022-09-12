471ee1bcf50d33a71a87f58ad6991bd887287d4d
Xine75 <cjdoza@gmail.com>
2022-09-12 15:55:19 -0500

    error handling

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,23 +1,39 @@
-import {defineEventHandler} from "h3";
+
+
 import { db } from "../../db";
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
+import { sendError } from "h3";
 
 export default defineEventHandler(async (e) => {
     const method = e.req.method;
 
-    if(method === "GET") {
-        return db.todos
+    if (method === "GET") {
+        console.log(db.todos);
+        return db.todos;
     }
-    if(method === "POST") {
-        const body = await useBody(e)
-        if (!body.item) throw new Error()
+
+    if (method === "POST") {
+        const body = await useBody(e);
+
+        if (!body.item) {
+            const TodoNotFoundError = createError({
+                statusCode: 400,
+                statusMessage: "No item provided",
+                data: {},
+            });
+
+            sendError(e, TodoNotFoundError);
+        }
+
         const newTodo = {
             id: uuid(),
             item: body.item,
-            status: false
-        }
-        db.todos.push(newTodo)
-        return newTodo
-    }
+            completed: false,
+        };
 
-})
+        db.todos.push(newTodo);
+
+        return newTodo;
+    }
+});
+
